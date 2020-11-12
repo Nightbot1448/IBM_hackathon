f3e20921e1a67c3515bf9daef4b96fc40079219e
Piskunov Yaroslav <kinglordlear@gmail.com>
2020-11-12 20:03:13 +0300

    Tap event coords

--- app/app.py
+++ app/app.py
@@ -5,9 +5,11 @@ HERE_API_KEY = os.getenv('HERE_API_KEY')
 
 app = Flask(__name__, static_folder="./../static/", template_folder="./../template/")
 
+
 @app.route('/')
 def map_func():
-	return render_template('map.html',apikey=HERE_API_KEY)
+    return render_template('map.html', apikey=HERE_API_KEY)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
