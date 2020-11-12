ba3a9bfd7c4935b1c9f6bc01d56e31c0cf973949
Piskunov Yaroslav <kinglordlear@gmail.com>
2020-11-12 20:39:54 +0300

    Coords sent to server

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 HERE_API_KEY = os.getenv('HERE_API_KEY')
@@ -9,6 +9,13 @@ app = Flask(__name__, static_folder="./../static/", template_folder="./../templa
 @app.route('/')
 def map_func():
     return render_template('map.html', apikey=HERE_API_KEY)
+
+
+@app.route('/tapped', methods=['POST'])
+def func1():
+    data = request.get_json()
+    print(data)
+    return '123'
 
 
 if __name__ == "__main__":
