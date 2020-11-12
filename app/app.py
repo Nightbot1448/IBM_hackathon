f49803a739a648fd694db97e03cab91ae11d02da
Nightbot1448 <night1337bot@gmail.com>
2020-11-12 21:22:08 +0300

    get state by (lat,lng) and send to front

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
 import os
+import jsons
+import geopandas
+from shapely.geometry import Point
+from flask import Flask, render_template, request
 
 HERE_API_KEY = os.getenv('HERE_API_KEY')
 
 app = Flask(__name__, static_folder="./../static/", template_folder="./../template/")
 
+geodata = geopandas.read_file('static/states.json')
 
 @app.route('/')
 def map_func():
@@ -15,7 +19,9 @@ def map_func():
 def func1():
     data = request.get_json()
     print(data)
-    return '123'
+    tap = Point(float(data.get('lng')), float(data.get('lat')))
+    resp = geodata[['NAME', 'STATE']][geodata.geometry.map(tap.within)]
+    return resp.iloc[0].to_dict()
 
 
 if __name__ == "__main__":
