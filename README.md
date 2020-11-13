6e7e2cc0e9c70738992c5d4ed70e70fe20c241a4
justaleaf <nik.vag@mail.ru>
2020-11-14 02:35:40 +0300

    Connect form and model

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # IBM_hackathon
 
 > export HERE_API_KEY='\<api key\>'
+> export WATSON_API_KEY='\<api key\>'
--- app/app.py
+++ app/app.py
@@ -3,15 +3,20 @@ import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
 from flask import Flask, render_template, request, redirect
+import requests
 
 
 HERE_API_KEY = os.getenv('HERE_API_KEY')
+WATSON_API_KEY = os.getenv('WATSON_API_KEY')
 
-app = Flask(__name__, static_folder="./../static/", template_folder="./../template/")
+app = Flask(__name__, static_folder="./../static/",
+            template_folder="./../template/")
 
 geodata = gpd.read_file('static/states.json').set_index('STATE')
 stat = pd.read_csv('static/data.csv', index_col='Code')
-merged = stat.merge(geodata, left_index=True, right_index=True, how='outer').drop(columns=['NAME'])
+merged = stat.merge(geodata, left_index=True, right_index=True,
+                    how='outer').drop(columns=['NAME'])
+
 
 @app.route('/')
 def map_func():
@@ -21,16 +26,51 @@ def map_func():
 @app.route('/tapped', methods=['POST'])
 def func1():
     data = request.get_json()
-    #print(data)
+    # print(data)
     tap = Point(float(data.get('lng')), float(data.get('lat')))
-    mask = merged.geometry.map(tap.within)    
-    #print(resp)
+    mask = merged.geometry.map(tap.within)
+    # print(resp)
     if mask.any():
         resp = merged[mask].iloc[0, merged.columns != 'geometry']
         resp['clust'] = int(resp['clust'])
         return render_template('test.html', state_data=resp.to_dict())
     else:
         return 'Not a State'
+
+
+@app.route('/submitted', methods=['POST'])
+def func2():
+    data = request.get_json()
+    # print(data)
+    token_response = requests.post('https://iam.eu-de.bluemix.net/identity/token', data={
+                                "apikey": WATSON_API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+    #print(token_response.json())
+    mltoken = token_response.json()["access_token"]
+
+    header = {'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + mltoken}
+
+    payload_scoring = {
+        "input_data": [
+            {
+                "fields":
+                    ['Clear_Days', 'mPS(20m)', 'Petroleum-Fired(%)', 'Natural_Gas-Fired(%)',
+                    'Coal-Fired(%)', 'Nuclear(%)', 'Renewables(%)'],
+                "values": [
+                    list(map(float, data))
+                        ]
+            }
+        ]
+    }
+    print(list(map(float, data)))
+    response_scoring = requests.post('https://eu-de.ml.cloud.ibm.com/ml/v4/deployments/d4e33a8e-ca69-48d2-b8b9-b0be8791e3d8/predictions?version=2020-11-13',
+                                    json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    print("Scoring response")
+    scoring = response_scoring.json()
+    scoring['predictions'][0]['values'][0][1][0] = scoring['predictions'][0]['values'][0][1][0].__round__(3)
+    print(scoring['predictions'][0]['values'][0][1][0])
+    # print(resp)
+    return render_template('model_result.html', response_scoring=scoring['predictions'][0]['values'][0])
 
 
 if __name__ == "__main__":
